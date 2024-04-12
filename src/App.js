325fd225a5d40180d8af3ac682c9ebf5cd3b8f88
MatkovskyA <a.matkovsky14@gmail.com>
2024-04-12 11:16:30 +0300

    Tested git comands

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Все получается и моя работа придет когда я буду готов
         </a>
       </header>
     </div>
